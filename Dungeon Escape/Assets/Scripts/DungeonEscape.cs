b8508d1a6bd05bfa567f4cb0e739ff7d2aaae05d
valdmel <valmneves92@gmail.com>
2020-09-19 22:02:02 -0300

    Upload project

---
+++ Dungeon Escape/Assets/Scripts/DungeonEscape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonEscape : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    State[] nextStates;
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.getStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeState();
+    }
+
+    public void changeState() {
+        var nextStates = state.getNextStates();
+
+        for (int i = 0; i < nextStates.Length; i++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i)) {
+                state = nextStates[i];
+            }
+        }
+
+        //playStateAudio(state.getStateAudioClips()[0]);
+        textComponent.text = state.getStateStory();
+    }
+}
---
+++ Dungeon Escape/Assets/Scripts/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)][SerializeField] public string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string getStateStory() {
+        return storyText;
+    }
+
+    public State[] getNextStates() {
+        return nextStates;
+    }
+}
